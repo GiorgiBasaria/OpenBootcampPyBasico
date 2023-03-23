4ecbaebbc7d490ea109cfafa4251bebfd20d4400
GiorgiBasaria <86610362+GiorgiBasaria@users.noreply.github.com>
2023-03-23 16:28:34 +0100

    Add files via upload

---
+++ ejer8pyBasico/ejer8pyBasico.py
@@ -0,0 +1,15 @@
+archivo = open("ejemplo.txt", "w")
+
+archivo = open("ejemplo.txt", "w")
+
+
+archivo.write("Esto es el ejemplo de archivo de texto. \n")
+
+archivo.close()
+
+archivo = open("ejemplo.txt", "a")
+
+archivo.write("Aqui se agrega mas contenido al arcchivo. \n")
+
+
+archivo.close()
